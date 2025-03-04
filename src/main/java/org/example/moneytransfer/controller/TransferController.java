9713f721f00c5b5b6e2d8c9415c9b5e35bcb3e0f
DenisAbramenko <den-a96@mail.ru>
2025-03-05 00:00:03 +0300

    modified TransferController

--- src/main/java/org/example/moneytransfer/controller/TransferController.java
+++ src/main/java/org/example/moneytransfer/controller/TransferController.java
@@ -1,16 +1,15 @@
 package org.example.moneytransfer.controller;
 
-import org.example.moneytransfer.model.ConfirmOperationRequest;
-import org.example.moneytransfer.model.TransferRequest;
-import org.example.moneytransfer.model.TransferResponse;
+import org.example.moneytransfer.entity.ConfirmOperation;
+import org.example.moneytransfer.entity.Transfer;
 import org.example.moneytransfer.service.TransferService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/transfer")
 public class TransferController {
 
     private final TransferService transferService;
@@ -20,14 +19,12 @@ public class TransferController {
     }
 
     @PostMapping("/transfer")
-    public ResponseEntity<TransferResponse> transferMoney(@RequestBody TransferRequest request) {
-        TransferResponse response = transferService.transferMoney(request);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public Object transferMoney(@RequestBody Transfer request) {
+        return transferService.transferMoney(request);
     }
 
     @PostMapping("/confirmOperation")
-    public ResponseEntity<TransferResponse> confirmOperation(@RequestBody ConfirmOperationRequest request) {
-        TransferResponse response = transferService.confirmOperation(request);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public Object confirmOperation(@RequestBody ConfirmOperation request) {
+        return transferService.confirmOperation(request);
     }
 }
