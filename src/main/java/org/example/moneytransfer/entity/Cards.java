b7ec14d2d245e8765eb2e6dfa7ecef04e1c35b37
DenisAbramenko <den-a96@mail.ru>
2025-03-08 23:45:10 +0300

    add entity Cards.java

---
+++ src/main/java/org/example/moneytransfer/entity/Cards.java
@@ -0,0 +1,21 @@
+package org.example.moneytransfer.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cards {
+    @Id
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "valid_till")
+    private String validTill;
+
+    @Column(name = "cvv")
+    private String cvv;
+
+    @Column(name = "balance")
+    private int balance;
+}
