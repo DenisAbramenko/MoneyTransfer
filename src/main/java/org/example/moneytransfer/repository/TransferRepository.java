e77a7b1101cb46b265205eccf2138a5e38083f76
DenisAbramenko <den-a96@mail.ru>
2025-03-08 18:11:00 +0300

    modified TransferRepository & TransferService

--- src/main/java/org/example/moneytransfer/repository/TransferRepository.java
+++ src/main/java/org/example/moneytransfer/repository/TransferRepository.java
@@ -4,17 +4,10 @@ import org.example.moneytransfer.entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
-
+    @Override
     Optional<Transfer> findById(Long id);
-
-    List<Transfer> findByStatus(String status);
-
-    List<Transfer> findByCardFromNumber(String cardFromNumber);
-
-    List<Transfer> findByCardToNumber(String cardToNumber);
 }
--- src/main/java/org/example/moneytransfer/service/TransferService.java
+++ src/main/java/org/example/moneytransfer/service/TransferService.java
@@ -1,9 +1,7 @@
 package org.example.moneytransfer.service;
 
-import org.example.moneytransfer.dto.Transfer;
-import org.example.moneytransfer.model.ConfirmOperationRequest;
-import org.example.moneytransfer.model.TransferRequest;
-import org.example.moneytransfer.model.TransferResponse;
+import org.example.moneytransfer.entity.ConfirmOperation;
+import org.example.moneytransfer.entity.Transfer;
 import org.example.moneytransfer.repository.TransferRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +10,7 @@ import org.springframework.stereotype.Service;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TransferService {
@@ -25,19 +22,27 @@ public class TransferService {
         this.transferRepository = transferRepository;
     }
 
-    public TransferResponse transferMoney(TransferRequest request) {
+    public String transferMoney(Transfer request) {
+        String operationId = UUID.randomUUID().toString();
+        request.setOperationId(operationId);
+        request.setTransferDate(LocalDateTime.now());
+
+        transferRepository.save(request);
+
+
+
         logTransaction(request, "SUCCESS");
-        return new TransferResponse("operation-id-123");
+        return request.getOperationId();
     }
 
-    public TransferResponse confirmOperation(ConfirmOperationRequest request) {
-        return new TransferResponse("operation-id-123");
+    public String confirmOperation(ConfirmOperation request) {
+        return request.getOperationId();
     }
 
-    private void logTransaction(TransferRequest request, String result) {
+    private void logTransaction(Transfer request, String result) {
         String logEntry = String.format("Date: %s, From Card: %s, To Card: %s, Amount: %d, Result: %s",
                 LocalDateTime.now(), request.getCardFromNumber(), request.getCardToNumber(),
-                request.getAmount().getValue(), result);
+                request.getAmountValue(), result);
 
         // Логирование в файл
         try (FileWriter writer = new FileWriter("operations.log", true)) {
@@ -45,20 +50,5 @@ public class TransferService {
         } catch (IOException e) {
             logger.error("Failed to write log", e);
         }
-    }
-
-    // Метод для сохранения перевода
-    public Transfer saveTransfer(Transfer transfer) {
-        return transferRepository.save(transfer);
-    }
-
-    // Метод для поиска перевода по ID
-    public Optional<Transfer> getTransferById(Long id) {
-        return transferRepository.findById(id);
-    }
-
-    // Метод для поиска переводов по статусу
-    public List<Transfer> getTransfersByStatus(String status) {
-        return transferRepository.findByStatus(status);
     }
 }
